08745f7fee2191418bf2e4d39d3b7cfa83c0ff82
Gabriel <gabrieldubois.eng@gmail.com>
2023-06-14 21:31:57 -0400

    longest consecutive

---
+++ Arrays & Hashing/Longest_Consecutive_Sequence/Solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        nums = list(set(nums))
+        nums.sort()
+        if len(nums) == 0:
+            return 0
+        count, result = 1, 1
+        for i in range(len(nums) - 1):
+            if nums[i + 1] == (nums[i] + 1):
+                count += 1
+                if result < count:
+                    result = count
+            else:
+                if result < count:
+                    result = count
+                count = 1
+        return result
